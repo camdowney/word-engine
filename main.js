06479d8d1cd4390da447c49fa70bf259e4127f1e
Cameron Downey <camadowney@gmail.com>
2022-10-04 13:44:40 -0500

    Begin filtering

--- main.js
+++ main.js
@@ -9,11 +9,41 @@ const suggestionsBox = render(suggestionsCol, '<div class="suggestions-box"></di
 
 const NUM_CELLS = 30
 let cells = []
+let letters = []
 let index = 0
 
-forNum(NUM_CELLS, () =>
-  cells.push(render(cellsBox, '<div class="cell"></div>'))
+forNum(NUM_CELLS, (_, index) =>
+  cells.push(render(cellsBox, `<div class="cell" data-index="${index}" data-state="0"></div>`))
 )
+
+const fiveLetterWords = dictionary.filter(word => word.length === 5)
+renderSuggestions(fiveLetterWords)
+
+cellsBox.addEventListener('click', e => {
+  const cell = e.srcElement
+  const cellIndex = cell.dataset.index
+
+  if (!cellIndex) return
+  if (letters[cellIndex]) cell.dataset.state = (cell.dataset.state + 1) % 3
+
+  let lettersNotAt = []
+  let lettersAt = []
+
+  cells.forEach(cell => {
+    if (cell.innerHTML.length === 1) {
+      const state = cell.dataset.state
+
+      if (state === 1) {
+
+      }
+    }
+  })
+
+  console.log(lettersNotAt, lettersAt)
+
+  const filteredWords = fiveLetterWords.filter(Boolean)
+  renderSuggestions(filteredWords)
+})
 
 window.addEventListener('keydown', e => {
   if (e.repeat) return
@@ -22,20 +52,37 @@ window.addEventListener('keydown', e => {
 
   if (key === 'BACKSPACE' && index > 0) {
     cells[--index].innerHTML = ''
+    cells[index].dataset.state = 0
+    letters[index] = ''
     return
   }
 
-  if (isLetter(key) && index < NUM_CELLS) {
-    cells[index++].innerHTML = key
+  if (!isLetter(key)) return
+
+  if (index < NUM_CELLS) {
+    cells[index].innerHTML = key
+    letters[index++] = key
   }
+
+  // if (index % 5 > 0 || index === 0) {
+  //   index++
+  //   return
+  // }
+
+  // if (!possibleWords.includes(letters.slice(index - 5, 5).join(''))) {
+  //   return
+  // }
+
+  // index++
 })
 
-const possibleWords = filterWords({ length: 5 })
-suggestionsBox.innerHTML = ''
-render(suggestionsBox, `<p class="suggestions-header">Showing ${possibleWords.length} possible words</p>`)
-
-const suggestions = render(suggestionsBox, '<div class="suggestions"></div>')
-render(suggestions, possibleWords.map(w => `<p>${w}</p>`).join(''))
+function renderSuggestions(words) {
+  suggestionsBox.innerHTML = ''
+  render(suggestionsBox, `<p class="suggestions-header">Showing ${words.length} possible words</p>`)
+  
+  const suggestions = render(suggestionsBox, '<div class="suggestions"></div>')
+  render(suggestions, words.map(w => `<p>${w}</p>`).join(''))
+}
 
 function render(e, html) {
   if (!e || !html) return
@@ -47,10 +94,6 @@ function render(e, html) {
 function isLetter(str) {
   if (!str) return false
   return str.length === 1 && str.toLowerCase().match(/[a-z]/)
-}
-
-function filterWords(query) {
-  return dictionary.filter(word => word.length === query.length)
 }
 
 function forNum(num, callback) {
