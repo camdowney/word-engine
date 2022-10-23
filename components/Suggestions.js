a045254a597c5f89c2ac3216256a2b16633ce123
Cameron Downey <camadowney@gmail.com>
2022-10-23 00:44:43 -0500

    Reformat

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,15 +1,16 @@
 import { render } from '../min.js'
-import { getPageItems, getNumPages } from '../util.js'
+import { getPageItems, getNumPages } from '../lib/util.js'
 import { fiveLetterWords } from '../dictionary/fiveLetterWords.js'
-import filterWords from '../filterWords.js'
+import filterWords from '../lib/filterWords.js'
+import getFiltersFromCells from '../lib/getFiltersFromCells.js'
 
 const PAGE_SIZE = 100
 const SCROLL_OFFSET = 600
 
-export default function Suggestions({ filters }) {
-  const filteredWords = filterWords(fiveLetterWords, filters)
-  const allSuggestions = filteredWords.map(w => `<p>${w}</p>`)
-  const numPages = getNumPages(allSuggestions, PAGE_SIZE)
+export default function Suggestions({ cells }) {
+  const filtered = filterWords(fiveLetterWords, getFiltersFromCells(cells))
+  const all = filtered.map(w => `<p>${w}</p>`)
+  const numPages = getNumPages(all, PAGE_SIZE)
 
   let currentPage = 0
 
@@ -19,14 +20,14 @@ export default function Suggestions({ filters }) {
     if (list.scrollTop < list.scrollHeight - SCROLL_OFFSET) return
     if (currentPage === numPages) return list.removeEventListener('scroll', loadMoreSuggestions)
     
-    render(list, getPageItems(allSuggestions, currentPage++, PAGE_SIZE))
+    render(list, getPageItems(all, currentPage++, PAGE_SIZE))
   }
   
   return {
     id: 'suggestions',
     _mount: loadMoreSuggestions,
     c: [
-      `<p class="suggestions-header">Showing ${filteredWords.length} possible words</p>`,
+      `<p class="suggestions-header">Showing ${filtered.length} possible words</p>`,
       { id: 'suggestions-list', _scroll: loadMoreSuggestions },
     ],
   }
