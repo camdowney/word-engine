ce1f332a00d9b24f474527f84b8d202dd6c3d6c7
Cameron Downey <cameron@builtbyblacktop.com>
2022-10-27 14:35:13 -0500

    Remove rework code

--- components/Counter.js
+++ components/Counter.js
@@ -3,13 +3,6 @@ import { store } from '../min.js'
 export default function Counter() {
   const [count, setCount] = store(0)
 
-  console.log(
-    'div', { class: 'counter-outer', _click: () => setCount(count + 1) }, [
-      ('p', count),
-      (Counter2),
-    ]
-  )
-
   return {
     class: 'counter-outer',
     _click: () => setCount(count + 1),
