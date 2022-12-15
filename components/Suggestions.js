2cfa9b31bff6e8a03d4e58ac1bdcd263ad9ec7d1
Cameron Downey <camadowney@gmail.com>
2022-12-15 17:28:18 -0600

    Refactor Suggestions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -3,30 +3,29 @@ import { fiveLetterWords } from '../dictionary/fiveLetterWords.js'
 import filterWords from '../lib/filterWords.js'
 import getFiltersFromCells from '../lib/getFiltersFromCells.js'
 
-export default function Suggestions({ uid, cells }) {
+export default function Suggestions({ cells }) {
   const filtered = filterWords(fiveLetterWords, getFiltersFromCells(cells))
   const chunks = chunk(filtered, 100)
 
   let current = 0
 
-  const loadMoreSuggestions = () => {
-    const list = document.querySelector('#' + uid)
+  const loadMore = e => {
+    const list = e.srcElement
 
     if (list.scrollTop < list.scrollHeight - 600)
       return
 
     if (current === chunks.length)
-      return list.removeEventListener('scroll', loadMoreSuggestions)
+      return list.removeEventListener('scroll', loadMore)
     
     list.innerHTML += chunks[current++].map(word => `<p>${word}</p>`).join('')
   }
   
   return {
     class: 'suggestions',
-    _mount: loadMoreSuggestions,
     c: [
       { r: 'p', class: 'suggestions-header', c: `Showing ${filtered.length} possible words` },
-      { id: uid, class: 'suggestions-list', _scroll: loadMoreSuggestions },
+      { class: 'suggestions-list', _mount: loadMore, _scroll: loadMore },
     ],
   }
 }
