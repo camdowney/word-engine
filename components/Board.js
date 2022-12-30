e4e964f5a2413ba9ed073848c12551e013b09ffa
Cameron Downey <camadowney@gmail.com>
2022-12-29 20:51:12 -0600

    Update to Neutro 1.1.5

--- components/Board.js
+++ components/Board.js
@@ -7,8 +7,8 @@ export default function Board({ cells, _click }) {
     c: allCells.map((cell, i) => ({
       tag: cell?.state >= 0 ? 'button' : 'div',
       class: 'cell',
-      data_index: i,
-      data_state: cell?.state,
+      'data-index': i,
+      'data-state': cell?.state,
       c: [
         { class: 'cell-shape' },
         { class: 'cell-letter', c: cell?.letter },
