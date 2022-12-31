1ea60ce7e5d465229ffeb8bf569d2822d4bc51d9
Cameron Downey <camadowney@gmail.com>
2022-12-31 16:56:49 -0600

    Prevent commands like ctrl + c adding letters to the board

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Word Engine
-Word Engine is a demo application that integrates [Neutro](https://github.com/camdowney/neutro). It can be used to narrow down possible five-letter words in the popular web game Wordle. Simply type some letters and click the boxes to cycle through colors, and the suggestions will be filtered automagically.
+Word Engine is a utility application that integrates [Neutro](https://github.com/camdowney/neutro). It can be used to narrow down possible five-letter words in Wordle. Simply type some letters and click the boxes to cycle through colors, match the board to your current board in Wordle, and the suggestions will be filtered automatically.
 
 More documentation coming soon.
--- components/Layout.js
+++ components/Layout.js
@@ -5,31 +5,47 @@ import { isLetter } from '../lib/util.js'
 
 export default function Layout({ store }) {
   const cells = store([])
+  let holdingCtrlOrCmd = false
 
   const __keydown = e => {
     const key = e.key.toLowerCase()
 
+    if (key === 'control' || key === 'meta')
+      holdingCtrlOrCmd = true
+
+    if (holdingCtrlOrCmd)
+      return
+
     if (key === 'backspace' && cells().length > 0) {
-      cells(cells().slice(0, -1))
+      cells().pop()
     }
     else if (!e.repeat && isLetter(key) && cells().length < 30) {
-      cells([ ...cells(), { letter: key, state: 0 } ])
+      cells().push({ letter: key, state: 0 })
     }
+
+    cells(cells())
+  }
+
+  const __keyup = e => {
+    const key = e.key.toLowerCase()
+
+    if (key === 'control' || key === 'meta')
+      holdingCtrlOrCmd = false
   }
 
   const _click = e => {
     const index = e.srcElement.dataset.index
+    const cell = cells()[index]
 
-    if (!cells()[index])
+    if (!cell)
       return
 
-    let temp = cells()
-    temp[index].state = (temp[index].state + 1) % 3
-    cells(temp)
+    cell.state = (cell.state + 1) % 3
+    cells(cells())
   }
 
   return { 
-    tag: 'section', class: 'layout', __keydown, c: [
+    tag: 'section', class: 'layout', __keydown, __keyup, c: [
       { class: 'col', c: [
         { tag: Board, cells, _click },
         { tag: Keyboard },
