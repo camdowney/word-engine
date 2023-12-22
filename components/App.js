d171482c06dbcc2446339801e23f9f91ad16a312
Cameron Downey <camadowney@gmail.com>
2023-12-22 14:21:52 -0600

    Simplify

--- components/App.js
+++ components/App.js
@@ -1,27 +1,25 @@
-import { q, watch } from 'https://cdn.jsdelivr.net/npm/neutro@2.4.0/min.js'
+import { q } from 'https://cdn.jsdelivr.net/npm/neutro@2.4.0/min.js'
 import { Layout } from './Layout.js'
 
 const root = q('#root')
 
-watch(() => {
-  root.html`
-    <header>
-      <h1>Word Engine</h1>
-    </header>
-    <main>
-      ${Layout()}
-    </main>
-    <footer>
-      <p>
-        © ${new Date().getFullYear()} Cameron Downey | Built with
-        <a
-          href='https://github.com/camdowney/neutro'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Neutro
-        </a>
-      </p>
-    </footer>
-  `
-})
+root.html`
+  <header>
+    <h1>Word Engine</h1>
+  </header>
+  <main>
+    ${Layout()}
+  </main>
+  <footer>
+    <p>
+      © ${new Date().getFullYear()} Cameron Downey | Built with
+      <a
+        href='https://github.com/camdowney/neutro'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Neutro
+      </a>
+    </p>
+  </footer>
+`
